5626fa60310662515ad36ab99727a1733588b3b3
valmik <rsnorunt@gmail.com>
2018-04-15 19:47:56 -0700

    small edits

--- src/motor_interface.py
+++ src/motor_interface.py
@@ -5,17 +5,39 @@ from geometry_msgs.msg import Twist
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 import atexit
 
-# create the default object without changing I2C address or frequency
-mh = Adafruit_MotorHAT(addr=0x60)
+class Robot():
+    def __init__(self, left_id, right_id):
+        # create the default object without changing I2C address or frequency
+        self.mh = Adafruit_MotorHAT(addr=0x60)
+        self.left = self.mh.getMotor(left_id)
+        self.right = self.mh.getMotor(right_id)
 
-# For shutdown
-def turnOffMotors():
-    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+    # For shutdown
+    def turnOffMotors(self):
+        self.mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 
-atexit.register(turnOffMotors)
+        atexit.register(turnOffMotors)
 
-def low_level_control(currentVelocity, desiredVelocity):
+    # def low_level_control(currentVelocity, desiredVelocity):
+
+    def set_speed(sl, sr):
+        speed_left = max(-255, min(sl, 255))
+        speed_right = max(-255, min(sr, 255))
+
+        if speed_left < 0:
+            self.left.setSpeed(-speed_left)
+            self.left.run(Adafruit_MotorHAT.BACKWARD)
+        else:
+            self.left.setSpeed(speed_left)
+            self.left.run(Adafruit_MotorHAT.FORWARD)
+        if speed_right < 0:
+            self.right.setSpeed(-speed_right)
+            self.right.run(Adafruit_MotorHAT.BACKWARD)
+        else:
+            self.right.setSpeed(speed_right)
+            self.right.run(Adafruit_MotorHAT.FORWARD)
+
 
