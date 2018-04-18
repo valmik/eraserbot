c03b8e3b17dc3151a42d288b0f72ae996c35e9cd
valmik <rsnorunt@gmail.com>
2018-04-18 16:29:22 -0700

    finished serial write

---
+++ scripts/write_to_serial/write_to_serial.ino
@@ -0,0 +1,54 @@
+#include <Wire.h>
+
+/* Prints sensor information to serial
+ * for use on Teensy
+ * includes Encoder and IMU data
+ * last modified 4.16.2018
+ * Reads as "DISTANCE [mm],AcX,AcY,GyZ"
+ */
+
+#include <Encoder.h>
+#include <Wire.h>
+
+Encoder myEncL(14, 15);  // (pinA, pinB) on left encoder
+Encoder myEncR(10, 11);  // (pinA, pinB) on right encoder
+//   avoid using pins with LEDs attached
+
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+// SCL Pin 19, SDA Pin 18;
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+void setup() {
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  
+  long PosL = myEncL.read() * 0.0430;  // mm/count calibration
+  long PosR = myEncR.read() * 0.0430;  // mm/count calibration
+  
+  Serial.print(PosL); Serial.print(",");
+  Serial.print(PosR); Serial.print(",");
+  Serial.print(AcX);  Serial.print(",");
+  Serial.print(AcY);  Serial.print(",");
+  Serial.println(GyZ);
+  
+  delay(333);
+}
