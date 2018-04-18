e3943b00861ec3fbb1bdf2c069856b51134f36f6
valmik <rsnorunt@gmail.com>
2018-04-18 12:44:38 -0700

    robot class

--- src/motor_interface.py
+++ src/motor_interface.py
@@ -11,6 +11,8 @@ class Robot():
         self.mh = Adafruit_MotorHAT(addr=0x60)
         self.left = self.mh.getMotor(left_id)
         self.right = self.mh.getMotor(right_id)
+        atexit.register(turnOffMotors)
+
 
     # For shutdown
     def turnOffMotors(self):
@@ -19,23 +21,26 @@ class Robot():
         self.mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
         self.mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 
-        atexit.register(turnOffMotors)
-
     # def low_level_control(currentVelocity, desiredVelocity):
 
     def set_speed(sl, sr):
         speed_left = max(-255, min(sl, 255))
         speed_right = max(-255, min(sr, 255))
 
+
         if speed_left < 0:
             self.left.setSpeed(-speed_left)
             self.left.run(Adafruit_MotorHAT.BACKWARD)
+        elif speed_left == 0:
+            self.left.run(Adafruit_MotorHAT.RELEASE)
         else:
             self.left.setSpeed(speed_left)
             self.left.run(Adafruit_MotorHAT.FORWARD)
         if speed_right < 0:
             self.right.setSpeed(-speed_right)
             self.right.run(Adafruit_MotorHAT.BACKWARD)
+        elif speed_right == 0:
+            self.right.run(Adafruit_MotorHAT.RELEASE)
         else:
             self.right.setSpeed(speed_right)
             self.right.run(Adafruit_MotorHAT.FORWARD)
