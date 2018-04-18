18ca16911228b4ed6745c7504d05fa0cacddbe23
valmik <rsnorunt@gmail.com>
2018-04-18 15:28:06 -0700

    fixed serial code

--- scripts/serial_code/serial_code.ino
+++ scripts/serial_code/serial_code.ino
@@ -14,7 +14,7 @@ Encoder myEnc(14, 15); // (pinA, pinB) on encoder
 //   avoid using pins with LEDs attached
 
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
-// SCL Pin 19, SDA Pin 18
+// SCL Pin 19, SDA Pin 18;
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 
 void setup() {
@@ -32,9 +32,13 @@ void loop() {
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-  AcX = Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-  AcY = Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  GyZ = Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
   
   long Pos = myEnc.read() * 0.0430;  // mm/count calibration
   Serial.print(Pos); Serial.print(",");
