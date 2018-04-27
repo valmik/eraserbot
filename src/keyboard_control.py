3d89430da591f3aea0f660cb058f0ff7c8779789
valmik <rsnorunt@gmail.com>
2018-04-27 15:23:06 -0700

    tried another thing

--- src/keyboard_control.py
+++ src/keyboard_control.py
@@ -16,7 +16,7 @@ def getch():
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     try:
-        tty.setraws(fd)
+        tty.setraw(fd)
         ch = sys.stdin.read(1)
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
